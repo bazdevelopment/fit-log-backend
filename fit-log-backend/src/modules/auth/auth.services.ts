c15e603fa09b4de43910a749ef5d1c3b639ab325
Baziluc Marian <bazilucmarian98@yahoo.com>
2023-12-26 11:46:27 +0200

    fitlog-10-change returned types

--- fit-log-backend/src/modules/auth/auth.services.ts
+++ fit-log-backend/src/modules/auth/auth.services.ts
@@ -15,7 +15,7 @@ import JWT from "jsonwebtoken";
  */
 export const signUpUserService = async (
   userInfo: TSignUpUser
-): Promise<TSignUpUserResponse> => {
+): Promise<TSignUpUserResponse | undefined> => {
   try {
     const { password, otpCode, ...restUserInfo } = userInfo;
     const { hash, salt } = hashField(password);
@@ -35,7 +35,7 @@ export const signUpUserService = async (
     return result;
   } catch (error: unknown) {
     const errorResponse = error as Error;
-    return createHttpException(
+    createHttpException(
       HTTP_STATUS_CODE.BAD_REQUEST,
       `[signUpUserService]: ${errorResponse.message}`
     );
@@ -118,7 +118,7 @@ export const resendOtpCode = async ({
  */
 export const verifyOtpCode = async (
   email: string
-): Promise<TSignUpUserResponse | void> => {
+): Promise<TSignUpUserResponse | undefined> => {
   try {
     return await prisma.user.update({
       where: {
@@ -130,7 +130,7 @@ export const verifyOtpCode = async (
     });
   } catch (error) {
     const errorResponse = error as Error;
-    return createHttpException(
+    createHttpException(
       HTTP_STATUS_CODE.BAD_REQUEST,
       `[verifyOtpCode]: ${errorResponse.message}`
     );
