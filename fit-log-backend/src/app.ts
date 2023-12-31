9d0ae3a5431490bff3573145169b02f408bdb021
Baziluc Marian <bazilucmarian98@yahoo.com>
2023-12-31 13:06:38 +0200

    fitlog-25-covered PID processes

--- fit-log-backend/src/app.ts
+++ fit-log-backend/src/app.ts
@@ -2,6 +2,8 @@ import { buildServer } from "./config/server";
 import dotenv from "dotenv";
 dotenv.config();
 
+const signals = ["SIGINT", "SIGTERM"];
+
 async function main() {
   const app = await buildServer();
 
@@ -11,6 +13,23 @@ async function main() {
     port,
     host: process.env.HOST,
   });
+
+  /** The following code snippet demonstrates the implementation of a graceful shutdown mechanism for a Node.js application using signals. This ensures that the application handles termination signals gracefully, allowing it to perform necessary cleanup tasks before shutting down.
+   */
+  for (const signal of signals) {
+    process.on(signal, () => {
+      gracefulShutDownApp(app);
+    });
+  }
 }
 
 main();
+
+/**
+ * The gracefulShutDownApp function is responsible for performing the necessary cleanup and shutting down the application gracefully. In this specific example, it closes the application instance asynchronously.
+ */
+const gracefulShutDownApp = async (
+  app: Awaited<ReturnType<typeof buildServer>>
+) => {
+  await app.close();
+};
