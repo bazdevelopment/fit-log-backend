ebb9b346fc0e00ef1758141d0d1b00dcde922c90
Baziluc Marian <bazilucmarian98@yahoo.com>
2024-01-02 17:44:00 +0200

    filog-31-added a short documentation for a helper method

--- fit-log-backend/src/utils/registerSchemas.ts
+++ fit-log-backend/src/utils/registerSchemas.ts
@@ -2,6 +2,9 @@ import { FastifyInstance, RawServerDefault } from "fastify";
 import { IncomingMessage, ServerResponse } from "http";
 import { Logger } from "pino";
 
+/**
+ * Utility function used to register all json schemas linked to models
+ */
 export function registerSchemas(
   app: FastifyInstance<
     RawServerDefault,
